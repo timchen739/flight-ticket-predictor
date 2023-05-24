ff1673b66172ea9926c5fd5f7a885d82622debb3
Tim Chen <kobec0739@yahoo.com>
2023-05-24 15:51:13 -0700

    Added comments for DataModelContext class

--- src/main/java/datamodel/DataModelContext.java
+++ src/main/java/datamodel/DataModelContext.java
@@ -5,6 +5,16 @@ import service.FailingCaseOutputGenerator;
 import service.PassingCaseOutputGenerator;
 import service.ResultNotFoundOutputGenerator;
 
+/*
+* This class serves the purpose of configuring the model to generate needed fake outputs
+*
+* goodData: indicates if we want to generate a passing case outputs, true means passing output is needed
+*
+* noResultFoundForARoute: controls whether we want to include a mock result when the model
+* is not able to get calculated prices for a specific route, in another word no result found for a route
+* True means include no result in the generated output
+*
+* */
 public class DataModelContext {
     private boolean goodData = true;
     private boolean noResultFoundForARoute = false;
